be9c7c4c1f3de44a2c5683f636d7eccd7296bc5e
mcsworld <mcsworld.tw@gmail.com>
2015-09-12 14:26:02 +0800

    Update pymcs.py

--- API/pymcs.py
+++ API/pymcs.py
@@ -51,23 +51,5 @@ def send_data(access_token, deviceId, chn_id, value):
 		return False
 	return True
 
-if __name__ == '__main__':
-	app_key = "YOUR_APP_KEY"
-	app_secret = "YOUR_APP_SECRET"
-	email = "YOUR_EMAIL"
-	password = "YOUR_PASSWORD"
-
-	deviceId = "DEVICE_ID"
-	deviceKey = "DEVICE_KEY"
-
-	# Get user token from MCS
-	r = get_user_token(app_key, app_secret, email, password)
-	if r:
-		access_token = r['access_token']
-
- 		# Control GPIO
-		send_data(access_token, deviceId, "GPIO_00", 1)
-		time.sleep(1)
-		send_data(access_token, deviceId, "GPIO_01", 0)
 
 
